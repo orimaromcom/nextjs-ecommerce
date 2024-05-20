636ed8a61808e04ece2b5a41d37c75b6ecff5b04
orimaromcom <ori.marom10@gmail.com>
2024-05-20 12:14:34 +0300

    add functionality

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { PageHeader } from "../_components/PageHeader";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <PageHeader>Products</PageHeader>
+      <Button></Button>
+    </>
+  );
+}
